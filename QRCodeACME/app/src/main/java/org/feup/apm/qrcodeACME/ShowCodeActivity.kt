3b25ade5a44d1e994727518940c6e2e9584829d8
Joana Mesquita <up201907878@fe.up.pt>
2023-04-04 15:48:16 +0100

    QR codes are now being encrypted with correct key

--- QRCodeACME/app/src/main/java/org/feup/apm/qrcodeACME/ShowCodeActivity.kt
+++ QRCodeACME/app/src/main/java/org/feup/apm/qrcodeACME/ShowCodeActivity.kt
@@ -18,6 +18,7 @@ import java.nio.charset.Charset
 import java.nio.file.Files
 import java.security.*
 import java.security.spec.PKCS8EncodedKeySpec
+import java.security.spec.X509EncodedKeySpec
 import java.util.*
 import javax.crypto.Cipher
 import javax.security.auth.x500.X500Principal
@@ -28,23 +29,20 @@ private const val ISO_SET = "ISO-8859-1"
 
 class ShowCodeActivity : AppCompatActivity() {
   private var content = ""
-  private var generated = generateAndStoreKeys()
   @SuppressLint("SetTextI18n")
   override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(savedInstanceState)
     setContentView(R.layout.activity_show_code)
-    generateAndStoreKeys()
 
     val image = findViewById<ImageView>(R.id.img_code)
     val type = intent.getIntExtra("type", 0)
     val value = intent.getStringExtra("value") ?: ""
+
     findViewById<TextView>(R.id.tv_title).text =  "QR Code"
     thread {
       encodeAsBitmap(type, encryptContent(value).toString()).also { runOnUiThread { image.setImageBitmap(it) } }
     }
   }
-
-
 
   private fun getPrivate(filename: String): PrivateKey? {
     try{
@@ -60,15 +58,15 @@ class ShowCodeActivity : AppCompatActivity() {
     }
   }
 
+
   private fun encryptContent(content : String) : ByteArray {
     if (content.isEmpty()) return ByteArray(0)
     return try {
-      val prKey = getPrivate("privatekey.pem")
+      val prKey = getPrivate("privatekey.der")
       val result = Cipher.getInstance(Constants.ENC_ALGO).run {
         init(Cipher.ENCRYPT_MODE, prKey)
         doFinal(content.encodeToByteArray())
       }
-      Log.d("encrypted",result.toString())
       result
     }
     catch (e: Exception) {
@@ -78,56 +76,8 @@ class ShowCodeActivity : AppCompatActivity() {
   }
 
 
-  private fun decryptResult(content: ByteArray)  : ByteArray{
-    if (content.isEmpty() ) return ByteArray(0);
-    try {
-      val entry = KeyStore.getInstance(Constants.ANDROID_KEYSTORE).run {
-        load(null)
-        getEntry(Constants.keyname, null)
-      }
-      val puKey = (entry as KeyStore.PrivateKeyEntry).certificate.publicKey
-      val clear = Cipher.getInstance(Constants.ENC_ALGO).run {
-        init(Cipher.DECRYPT_MODE, puKey)
-        doFinal(content)
-      }
-      return clear
-
-    }
-    catch (e: Exception) {
-      Log.d("error dec",e.toString())
-      return ByteArray(0)
-    }
-  }
 
 
-
-
-  private fun generateAndStoreKeys(): Boolean {
-    try {
-
-      if (!generated){
-        val spec = KeyPairGeneratorSpec.Builder(this)
-          .setKeySize(Constants.KEY_SIZE)
-          .setAlias(Constants.keyname)
-          .setSubject(X500Principal("CN=" + Constants.keyname))
-          .setSerialNumber(BigInteger.valueOf(Constants.serialNr))
-          .setStartDate(GregorianCalendar().time)
-          .setEndDate(GregorianCalendar().apply { add(Calendar.YEAR, 10) }.time)
-          .build()
-
-        KeyPairGenerator.getInstance(Constants.KEY_ALGO, Constants.ANDROID_KEYSTORE).run {
-          initialize(spec)
-          generateKeyPair()
-        }
-      }
-    }
-
-    catch (ex: Exception) {
-      Log.d("error", ex.toString())
-      return false
-    }
-    return true
-  }
 
   private fun encodeAsBitmap(type: Int, str: String): Bitmap? {
     val result: BitMatrix
