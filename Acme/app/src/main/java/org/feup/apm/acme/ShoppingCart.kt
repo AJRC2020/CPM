802367b1d36c738cfcab88d8c8f22dff4cb87437
joaobcosta2001 <joaoassuncaoecosta@gmail.com>
2023-04-11 16:31:21 +0100

    fixed errors

--- Acme/app/src/main/java/org/feup/apm/acme/ShoppingCart.kt
+++ Acme/app/src/main/java/org/feup/apm/acme/ShoppingCart.kt
@@ -2,10 +2,15 @@ package org.feup.apm.acme
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ImageButton
 
 class ShoppingCart : AppCompatActivity() {
+    private val backButton by lazy { findViewById<ImageButton>(R.id.shoppingCartBackButton)}
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_shopping_cart)
+        backButton.setOnClickListener {
+            finish()
+        }
     }
 }
--- Acme/app/src/main/java/org/feup/apm/acme/Vouchers.kt
+++ Acme/app/src/main/java/org/feup/apm/acme/Vouchers.kt
@@ -2,10 +2,15 @@ package org.feup.apm.acme
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ImageButton
 
 class Vouchers : AppCompatActivity() {
+    private val backButton by lazy { findViewById<ImageButton>(R.id.vouchersBackButton)}
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_vouchers)
+        backButton.setOnClickListener {
+            finish()
+        }
     }
 }
