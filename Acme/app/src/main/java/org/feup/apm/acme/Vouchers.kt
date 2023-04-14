3b4e28d301fb6ad1cac4998487e5ca0d0d81204b
joaobcosta2001 <joaoassuncaoecosta@gmail.com>
2023-04-15 00:35:15 +0100

    added vouchers and started navbar

--- Acme/app/src/main/java/org/feup/apm/acme/Vouchers.kt
+++ Acme/app/src/main/java/org/feup/apm/acme/Vouchers.kt
@@ -1,41 +1,65 @@
 package org.feup.apm.acme
-
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.ImageButton
+import android.util.Log
+import android.view.LayoutInflater
+import android.view.View
+import android.widget.*
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.ContextCompat
+
 
 class Vouchers : AppCompatActivity() {
     private val backButton by lazy { findViewById<ImageButton>(R.id.vouchersBackButton)}
-
-    private val navbarReceiptsButton by lazy { findViewById<ImageButton>(R.id.vouchers_navbar_receipts_button)}
-    private val navbarQRCodeButton by lazy { findViewById<ImageButton>(R.id.vouchers_navbar_qrcode_button)}
-    private val navbarShoppingCartButton by lazy { findViewById<ImageButton>(R.id.vouchers_navbar_shopping_cart_button)}
-    private val navbarProfileButton by lazy { findViewById<ImageButton>(R.id.vouchers_navbar_profile_button)}
+    private val scrollView by lazy {findViewById<LinearLayout>(R.id.vouchersScrollViewLayout)}
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_vouchers)
+        val inflater:LayoutInflater = LayoutInflater.from(this)
 
 
         //Buttons
         backButton.setOnClickListener {
             finish()
         }
-        navbarReceiptsButton.setOnClickListener {
-            val intent = Intent(this, Receipts::class.java);
-            startActivity(intent);
+
+        //I don't know what will be the format of voucher get requests, but assuming is something like {date, code, amount, validity
+
+        fun addVoucher(date:String,code:String, amount:Int, valid:Boolean){
+            val voucherLayout: View = inflater.inflate(R.layout.voucher_layout, scrollView, false)
+
+            // Populate the views with the information from the item
+
+            // Populate the views with the information from the item
+            val dateTextView: TextView = voucherLayout.findViewById(R.id.voucherDateText)
+            dateTextView.text = getString(R.string.voucher_code_format,code)
+
+            val codeTextView: TextView = voucherLayout.findViewById(R.id.voucherCodeText)
+            codeTextView.text = getString(R.string.voucher_date_format,date)
+
+            val percentageTextView: TextView = voucherLayout.findViewById(R.id.voucherPercentageText)
+            percentageTextView.text = getString(R.string.voucher_percentage_format)
+
+            val voucherValidImage: ImageView = voucherLayout.findViewById(R.id.voucherValidImage)
+            if (valid){
+                voucherValidImage.setImageDrawable(ContextCompat.getDrawable(this,R.drawable.tick_icon))
+            }else{
+                voucherValidImage.setImageDrawable(ContextCompat.getDrawable(this,R.drawable.cross_icon))
+            }
+
+            // Add the frame view to the scroll view
+
+            // Add the frame view to the scroll view
+            scrollView.addView(voucherLayout)
         }
-        navbarQRCodeButton.setOnClickListener {
-            val intent = Intent(this, QRCodeActivity::class.java);
-            startActivity(intent);
-        }
-        navbarShoppingCartButton.setOnClickListener {
-            val intent = Intent(this, ShoppingCart::class.java);
-            startActivity(intent);
-        }
-        navbarProfileButton.setOnClickListener {
-            val intent = Intent(this, UserProfile::class.java);
-            startActivity(intent);
-        }
+
+        addVoucher("10-05-2024","2901-dadd-3214",15,true)
+        addVoucher("13-06-2024","3jkd-32dd-mmmm",15,false)
+        addVoucher("13-06-2024","3jkd-32dd-mmmm",15,false)
+        addVoucher("13-06-2024","3jkd-32dd-mmmm",15,false)
+        addVoucher("13-06-2024","3jkd-32dd-mmmm",15,false)
+
+
+
     }
 }
